ed073feb62f2c04edcc48d7e699f5459b4fcfe38
SURYA CHAUDHARY <suryachaudhary.216@gmail.com>
2024-06-04 14:58:30 +0530

    Subtotal Completed

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import "./App.css";
+import Header from "./Header";
+import Home from "./Home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React from "react";
+import Checkout from "./Checkout";
+
+function App() {
+  return (
+    <Router>
+      <div className="app">
+        <Header/>
+      <Routes>
+          <Route path="/" element={<Home />} />
+      </Routes>
+      <Routes>
+      <Route path="/checkout" element={<Checkout /> } />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDj4jK-2P3h5nNZmBU-Ts6-56lpq7_JtUQ",
+  authDomain: "clone-ec4b1.firebaseapp.com",
+  projectId: "clone-ec4b1",
+  storageBucket: "clone-ec4b1.appspot.com",
+  messagingSenderId: "135076260735",
+  appId: "1:135076260735:web:46e4869b59ab40fe4fc84a",
+  measurementId: "G-Y80MHGLNL4",
+};
